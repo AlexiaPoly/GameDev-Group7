15a4b0875f64d76d16a4ff982e6be26b616c3943
AlexiaPoly <alexiapolyzogopoulos@gmail.com>
2025-05-27 11:21:14 +1000

    coding

---
+++ Carrot Hop/Assets/npcwalk.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class npcwalk : MonoBehaviour {
+    public float speed = 2f;
+    public Rigidbody2D rb;
+    public LayerMask groundLayers;
+    public Transform groundCheck;
+
+    bool isFacingLeft = true;
+
+    RaycastHit2D hit;
+
+    private void Update() {
+        hit = Physics2D.Raycast(groundCheck.position, -transform.up, 1f, groundLayers);
+    }
+
+    private void FixedUpdate() {
+        if (hit.collider != false) {
+            if (isFacingLeft) {
+              rb.velocity = new Vector2(speed,rb.velocity.y); 
+            } else {
+              rb.velocity = new Vector2(-speed,rb.velocity.y);   
+            }
+        } else {
+            isFacingLeft = !isFacingLeft;
+            transform.localScale = new Vector3(-transform.localScale.x, 1f, 1f);
+        }
+    }
+    
+}
+
